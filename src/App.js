6f573703f61c7ebcd93e4b84f8da65c133ea76a1
Meldin Geljic <meldingeljic@Mac-mini.local>
2020-02-20 08:52:54 +0100

    Component and page structure

--- src/App.js
+++ src/App.js
@@ -7,10 +7,38 @@ import About from "./pages/AboutPage";
 import Products from "./pages/ProductsPage";
 import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
 import Default from "./pages/Default";
 
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import SideCart from "./components/SideCart";
+
 function App() {
-  return <h1>Selam from eCommerce</h1>;
+  return (
+    <>
+      {/* navbar, sidebar, cart, footer */}
+
+      <Navbar />
+      <Sidebar />
+      <SideCart />
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
